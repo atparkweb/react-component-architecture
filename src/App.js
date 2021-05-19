3c7239a2e21c23ec3ebb97931b2f21cd266f63da
Andrew Park <atpark333@gmail.com>
2021-05-19 19:54:08 +0900

    feature: Add component for adding list

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React, { useCallback, useRef, useState } from "react";
 import "./App.css";
 
 /*
-- [ ] Use map to render list
-  - [ ] Set valid 'key' attribute for items
-- [ ] console log renders on components
+- [X] Use map to render list
+  - [X] Set valid 'key' attribute for items
+- [X] console log renders on components
 - [ ] useRef
   - [ ] Make an Input/Add component
   - [ ] implement onChange + useState
   - [ ] refactor into useRef
     - [ ] Add ref to input
 - [ ] useEffect + useRef for direct DOM interaction
-  - [ ] Make people array a state value
-  - [ ] define add handler
-  - [ ] Refactor out Add component
-    - [ ] Lift state
+  - [ ] Focus and clear input after adding
 - [ ] useCallback for reducing re-renders
 */
 
@@ -37,13 +34,57 @@ const Greeting = ({
 
   return (
     <p onClick={handleClick}>
-      {greetings[lang]}, <strong style={{ color }}>{name}</strong>
+      {greetings[lang]}, <strong>{name}</strong>
     </p>
   );
 };
 
+const Add = ({ onAdd }) => {
+  console.log("Render Add");
+
+  const [nameToAdd, setNameToAdd] = useState();
+
+  const handleAdd = (e) => {
+    onAdd && onAdd(nameToAdd);
+  };
+
+  const handleChange = (e) => {
+    setNameToAdd(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+};
+
 const App = () => {
+  const defaultPeople = [
+    {
+      name: "Adam",
+      lang: "en",
+      color: "orange",
+      objectID: 0,
+    },
+    {
+      name: "Axel",
+      lang: "en",
+      color: "blue",
+      objectID: 1,
+    },
+    {
+      name: "Blaze",
+      lang: "en",
+      color: "red",
+      objectID: 2,
+    },
+  ];
+
   const [clicked, setClicked] = useState({ name: "nothing" });
+
+  const [people, setPeople] = useState(defaultPeople);
 
   console.log("Render App");
 
@@ -51,32 +92,27 @@ const App = () => {
     setClicked(clicked);
   };
 
-  const people = [
-    {
-      name: "Adam",
-      lang: "en",
-      color: "orange",
-    },
-    {
-      name: "Axel",
-      lang: "en",
-      color: "blue",
-    },
-    {
-      name: "Blaze",
-      lang: "en",
-      color: "red",
-    },
-  ];
+  const addPerson = (name) => {
+    setPeople(people.concat({ name, lang: "en", objectID: people.length }));
+  };
 
   return (
     <div className="App">
+      <Add onAdd={addPerson} />
       <p>
         <strong>{clicked.name}</strong> was clicked
       </p>
-      <Greeting name="Adam" lang="en" onClick={handleClick} />
-      <Greeting name="Axel" lang="en" onClick={handleClick} />
-      <Greeting name="Blaze" lang="en" onClick={handleClick} />
+      {people.map((person) => {
+        return (
+          <Greeting
+            key={person.objectID}
+            name={person.name}
+            lang={person.lang}
+            color={person.color}
+            onClick={handleClick}
+          />
+        );
+      })}
     </div>
   );
 };
