f4dd6033e742583cb0e520417bc21aa6814baa83
Andrew Park <atpark333@gmail.com>
2021-05-20 18:34:44 +0900

    fix: Clean up state errors and bugs

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import "./App.css";
 
-/*
-- [X] Use map to render list
-  - [X] Set valid 'key' attribute for items
-- [X] console log renders on components
-- [ ] useRef
-  - [ ] Make an Input/Add component
-  - [ ] implement onChange + useState
-  - [ ] refactor into useRef
-    - [ ] Add ref to input
-- [ ] useEffect + useRef for direct DOM interaction
-  - [ ] Focus and clear input after adding
-- [ ] useCallback for reducing re-renders
-*/
-
-const Greeting = React.memo(({ person, onClick }) => {
+const Greeting = ({ person, onClick }) => {
   const greetings = {
     en: "Hello",
     ja: "こんにちは",
   };
 
-  console.log("Render Greeting");
+  console.log("Render Greeting", person.name);
 
-  const handleClick = e => {
-    onClick && onClick(person);
+  const handleClick = event => {
+    onClick && onClick(person.name);
   };
 
   return (
     <p onClick={handleClick}>
-      {greetings[person.lang]}, <strong>{person.name}</strong> (
-      {person.objectID})
+      {greetings[person.lang] || "Hi"}, <strong>{person.name}</strong>
     </p>
   );
-});
+};
 
 const Add = React.memo(({ onAdd }) => {
   console.log("Render Add");
 
   const inputRef = useRef();
 
-  const handleAdd = e => {
+  const handleAdd = () => {
     if (onAdd) {
       onAdd(inputRef.current.value);
       inputRef.current.value = "";
@@ -58,71 +43,43 @@ const Add = React.memo(({ onAdd }) => {
 });
 
 const App = () => {
-  const defaultPeople = useMemo(
-    () => [
-      {
-        name: "Adam",
-        lang: "en",
-        color: "orange",
-        objectID: 1,
-      },
-      {
-        name: "Axel",
-        lang: "en",
-        color: "blue",
-        objectID: 2,
-      },
-      {
-        name: "Blaze",
-        lang: "en",
-        color: "red",
-        objectID: 3,
-      },
-    ],
-    []
-  );
-
   // a local ref variable to auto increment unique ID
   let currentID = useRef(3);
 
-  const [clicked, setClicked] = useState({});
+  const [clickedName, setClickedName] = useState("");
 
-  const [people, setPeople] = useState(defaultPeople);
+  const [people, setPeople] = useState([]);
 
   console.log("Render App");
 
-  const handleClick = useCallback(clicked => {
-    setClicked(clicked);
-  }, []);
+  const handleClick = name => {
+    setClickedName(name);
+  };
 
-  const addPerson = useCallback(
-    name => {
-      currentID.current += 1;
-      console.log(currentID);
-      setPeople(people.concat({ name, lang: "en", objectID: currentID }));
-    },
-    [people]
-  );
+  const addPerson = name => {
+    currentID.current += 1;
+    const list = people.concat({
+      name,
+      lang: "en",
+      objectID: currentID.current,
+    });
+    console.log(list);
+    setPeople(list);
+  };
 
   return (
     <div className="App">
       <Add onAdd={addPerson} />
       <p style={{ height: "3rem" }}>
-        {clicked.name ? (
+        {clickedName ? (
           <>
-            <strong>{clicked.name}</strong> was clicked
+            <strong>{clickedName}</strong> was clicked
           </>
         ) : null}
       </p>
-      {people.map(person => {
-        return (
-          <Greeting
-            key={person.objectID}
-            person={person}
-            onClick={handleClick}
-          />
-        );
-      })}
+      {people.map(person => (
+        <Greeting key={person.objectID} person={person} onClick={handleClick} />
+      ))}
     </div>
   );
 };
