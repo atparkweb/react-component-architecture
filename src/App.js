7ea82d2e78afc79224c81d5c21a5b394adade604
Andrew Park <atpark333@gmail.com>
2021-05-20 17:06:33 +0900

    refactor: Reduce unnecessary renders through memoization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import "./App.css";
 
 /*
@@ -15,12 +15,7 @@ import "./App.css";
 - [ ] useCallback for reducing re-renders
 */
 
-const Greeting = ({
-  name = "Human",
-  lang = "ja",
-  color = "black",
-  onClick,
-}) => {
+const Greeting = React.memo(({ person, onClick }) => {
   const greetings = {
     en: "Hello",
     ja: "こんにちは",
@@ -28,87 +23,102 @@ const Greeting = ({
 
   console.log("Render Greeting");
 
-  const handleClick = (e) => {
-    onClick && onClick({ name, lang, color });
+  const handleClick = e => {
+    onClick && onClick(person);
   };
 
   return (
     <p onClick={handleClick}>
-      {greetings[lang]}, <strong>{name}</strong>
+      {greetings[person.lang]}, <strong>{person.name}</strong> (
+      {person.objectID})
     </p>
   );
-};
+});
 
-const Add = ({ onAdd }) => {
+const Add = React.memo(({ onAdd }) => {
   console.log("Render Add");
 
-  const [nameToAdd, setNameToAdd] = useState();
+  const inputRef = useRef();
 
-  const handleAdd = (e) => {
-    onAdd && onAdd(nameToAdd);
-  };
-
-  const handleChange = (e) => {
-    setNameToAdd(e.target.value);
+  const handleAdd = e => {
+    if (onAdd) {
+      onAdd(inputRef.current.value);
+      inputRef.current.value = "";
+    } else {
+      console.warn("onAdd prop is not defined for Greeting instance");
+    }
   };
 
   return (
     <div>
-      <input type="text" onChange={handleChange} />
+      <input type="text" ref={inputRef} />
       <button onClick={handleAdd}>Add</button>
     </div>
   );
-};
+});
 
 const App = () => {
-  const defaultPeople = [
-    {
-      name: "Adam",
-      lang: "en",
-      color: "orange",
-      objectID: 0,
-    },
-    {
-      name: "Axel",
-      lang: "en",
-      color: "blue",
-      objectID: 1,
-    },
-    {
-      name: "Blaze",
-      lang: "en",
-      color: "red",
-      objectID: 2,
-    },
-  ];
+  const defaultPeople = useMemo(
+    () => [
+      {
+        name: "Adam",
+        lang: "en",
+        color: "orange",
+        objectID: 1,
+      },
+      {
+        name: "Axel",
+        lang: "en",
+        color: "blue",
+        objectID: 2,
+      },
+      {
+        name: "Blaze",
+        lang: "en",
+        color: "red",
+        objectID: 3,
+      },
+    ],
+    []
+  );
 
-  const [clicked, setClicked] = useState({ name: "nothing" });
+  // a local ref variable to auto increment unique ID
+  let currentID = useRef(3);
+
+  const [clicked, setClicked] = useState({});
 
   const [people, setPeople] = useState(defaultPeople);
 
   console.log("Render App");
 
-  const handleClick = (clicked) => {
+  const handleClick = useCallback(clicked => {
     setClicked(clicked);
-  };
+  }, []);
 
-  const addPerson = (name) => {
-    setPeople(people.concat({ name, lang: "en", objectID: people.length }));
-  };
+  const addPerson = useCallback(
+    name => {
+      currentID.current += 1;
+      console.log(currentID);
+      setPeople(people.concat({ name, lang: "en", objectID: currentID }));
+    },
+    [people]
+  );
 
   return (
     <div className="App">
       <Add onAdd={addPerson} />
-      <p>
-        <strong>{clicked.name}</strong> was clicked
+      <p style={{ height: "3rem" }}>
+        {clicked.name ? (
+          <>
+            <strong>{clicked.name}</strong> was clicked
+          </>
+        ) : null}
       </p>
-      {people.map((person) => {
+      {people.map(person => {
         return (
           <Greeting
             key={person.objectID}
-            name={person.name}
-            lang={person.lang}
-            color={person.color}
+            person={person}
             onClick={handleClick}
           />
         );
