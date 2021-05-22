2091eeac6568d42ef2e473e1ae4ea1a6bef95f19
Andrew Park <atpark333@gmail.com>
2021-05-22 15:14:17 +0900

    Add examples of class v functional methods

--- src/classVsFunctional/ClassComponent.js
+++ src/classVsFunctional/ClassComponent.js
@@ -1,15 +1,33 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-  // - [ ] constructor(props)
-  //    - [ ]  super(props);
-  //    - [ ] init state
-  //    - [ ] bind event handlers
-  // ---
-  // - [ ] componentDidMount() {
-  // - [ ] componentWillUnmount()
-  // - [ ] componentDidUpdate
-  // - [ ] render
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: props.total,
+    };
+
+    this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this);
+  }
+
+  componentDidMount() {
+    console.log("Component added");
+  }
+  componentWillUnmount() {
+    console.log("Component deleting");
+  }
+  componentDidUpdate() {
+    console.log("Component re-render on change");
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>Hello</div>;
+  }
 }
 
 export default MyComponent;
--- src/classVsFunctional/FnComponent.js
+++ src/classVsFunctional/FnComponent.js
@@ -1,5 +1,30 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-function MyComponent() {}
+function MyComponent(props) {
+  const [someData, setSomeData] = useState({
+    total: 100,
+  });
+
+  // comp did mount
+  useEffect(() => {
+    // do something once on first load only
+    console.log("Component added");
+
+    // this is the destroy handler. runs when component is removed
+    return () => console.log("deleting");
+  }, []);
+
+  useEffect(() => {
+    // This runs on every render (update)
+  });
+
+  useEffect(() => {
+    // This runs when someData state changes
+  }, [someData]);
+
+  const handleClick = () => {};
+
+  return <div onClick={handleClick}>Total: {props.total}</div>;
+}
 
 export default MyComponent;
