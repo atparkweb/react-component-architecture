ca372214095e3cbc3893a2b8110f84da7a9a046d
Andrew Park <atpark333@gmail.com>
2021-05-22 11:42:00 +0900

    feature: Add class v. functional code

---
+++ src/classVsFunctional/ClassComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  // - [ ] constructor(props)
+  //    - [ ]  super(props);
+  //    - [ ] init state
+  //    - [ ] bind event handlers
+  // ---
+  // - [ ] componentDidMount() {
+  // - [ ] componentWillUnmount()
+  // - [ ] componentDidUpdate
+  // - [ ] render
+}
+
+export default MyComponent;
